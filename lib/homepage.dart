83a8e793ec3a227a086ee777f91c6dd6aa1a0cee
Varun8269 <varun1225789@gmail.com>
2021-07-27 21:03:09 +0530

    First commit

---
+++ lib/homepage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(250.0),
+            child: Text('Welcome to Home Page'),
+          ),
+        ),
+      ),
+    );
+  }
+}
